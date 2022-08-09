05c7805c15ae7b12f3e80807eb2b73c05e8b5814
sky line <32512284+linesky@users.noreply.github.com>
2022-08-09 20:09:15 +0100

    strcpy sample

---
+++ strcpy.asm
@@ -0,0 +1,25 @@
+JMP start
+hello: DB "Hello World!" ; Variable
+       DB 0	; String terminator
+
+start:
+	MOV C, hello    ; Point to var 
+	MOV D, 232	; Point to output
+	CALL strcpy
+        HLT             ; Stop execution
+
+strcpy:			; print(C:*from, D:*to)
+	PUSH A
+	PUSH B
+	MOV B, 0
+strcpy0:
+	MOV A, [C]	; Get char from var
+	MOV [D], A	; Write to output
+	INC C
+	INC D  
+	CMP B, [C]	; Check if end
+	JNZ strcpy0	; jump if not
+
+	POP B
+	POP A
+	RET
