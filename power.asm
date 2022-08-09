0c1ee57b046e65b74dbcde6586bd9fb9d2be74ff
sky line <32512284+linesky@users.noreply.github.com>
2022-08-09 18:58:31 +0100

    power sample code

---
+++ power.asm
@@ -0,0 +1,54 @@
+;https://schweigi.github.io/assembler-simulator/
+mov b,0
+mov a,2
+call power
+
+
+hlt
+
+mul:
+push a
+push c
+push d
+mov c,0
+mul0:
+cmp b,0
+jz mul1
+add c,a,c
+dec b
+cmp b,0
+jnz mul0
+mul1:
+mov b,c
+pop d
+pop c
+pop a
+ret
+
+power:
+push b
+push c
+push d
+mov c,a
+cmp b,0
+jz power1
+dec b
+power0:
+push b
+mov b,c
+call mul
+mov c,b
+pop b
+dec b
+cmp b,0
+jnz power0
+jmp power2
+power1:
+mov c,1
+power2:
+mov a,c
+pop d
+pop c
+pop b
+ret
+
