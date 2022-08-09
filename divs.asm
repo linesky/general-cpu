26be1eac34b062f4f9db31acc0ab4a3873d13fad
sky line <32512284+linesky@users.noreply.github.com>
2022-08-09 16:40:25 +0100

    divs sample

---
+++ divs.asm
@@ -0,0 +1,43 @@
+;https://schweigi.github.io/assembler-simulator/
+mov b,1
+mov c,3
+mov d,232
+mov a,9
+again:
+push b
+call div
+add b,48
+mov [d],b
+pop b
+inc b
+inc d
+dec c
+cmp c,0
+jnz again
+
+hlt
+
+div:
+push a
+push c
+push d
+mov c,0
+div0:
+cmp b,0
+jc div1
+cmp a,b
+ja div2
+push c
+sub c,a,b
+mov a,c
+pop c
+inc c
+cmp a,b
+jnc div0
+div1:
+div2:
+mov b,c
+pop d
+pop c
+pop a
+ret
